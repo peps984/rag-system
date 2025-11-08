5a16861069b3c2362d9df1365de259aa1a80baf6
peps984 <peppe984@gmail.com>
2025-11-08 16:49:18 +0100

    add .env and edit variables on settings file

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # app settings
-APP_NAME = "RAG System"
-APP_VERSION = "0.1.0"
-API_HOST = "0.0.0.0"
-API_PORT = 8000
+APP_NAME = os.getenv("APP_NAME", "RAG System")
+APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
+API_HOST = os.getenv("API_HOST", "0.0.0.0")
+API_PORT = int(os.getenv("API_PORT", 8000))
